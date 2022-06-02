6e49dd595bf97de7e6423a5a79004d61d9f68efe
Kartik Chauhan <kckartik17@gmail.com>
2022-06-02 19:13:47 +0530

    part 2
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,84 +1,47 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer">
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             Edit <code>src/App.js</code> and save to reload.
-//           </p>
-//           <a
-//             className="App-link"
-//             href="https://reactjs.org"
-//             target="_blank"
-//             rel="noopener noreferrer">
-//             Learn React
-//           </a>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
-
-// const App = () => {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer">
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer">
-        Learn React
-      </a>
-    </header>
-  </div>
+const Body = (props) => (
+  <p className="App-intro">
+    <div>{props.text}</div>
+    <div>{props.myFunc(50, 10)}</div>
+  </p>
 );
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <h1 className="App-title">{this.props.title}</h1>
+        <h1 className="App-title">{this.props.num}</h1>
+        <h1 className="App-title">{this.props.myArr[0]}</h1>
+        <h1 className="App-title">{JSON.stringify(this.props.myObj)}</h1>
+        <h1 className="App-title">
+          {JSON.stringify(this.props.myFunc(10, 20))}
+        </h1>
+      </header>
+    );
+  }
+}
+
+class App extends Component {
+  add(a, b) {
+    return a + b;
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header
+          title="Hello"
+          num={5}
+          myArr={[10, 20, 30]}
+          myObj={{ hello: "world", hello2: "world2" }}
+          myFunc={(a, b) => a + b}
+        />
+        <Body text="I am cool" myFunc={this.add} />
+      </div>
+    );
+  }
+}
 
 export default App;
