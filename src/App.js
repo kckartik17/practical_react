2fce7dd4ccd392b0184a452a1702c1f89a308f7e
Kartik Chauhan <kckartik17@gmail.com>
2022-06-03 13:01:37 +0530

    setState
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Header } from "./components/Header";
-import { Body, Body2 } from "./components/Body";
-import Counter from "./components/Counter";
+import ImageSlider from "./components/ImageSlider";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter initialCount={0} />
-        <Counter initialCount={100} />
+        <ImageSlider />
       </div>
     );
   }
