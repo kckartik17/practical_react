bbb1a6fac65a6ad0d6c50ab660455719e3c392d7
Kartik Chauhan <kckartik17@gmail.com>
2022-06-03 13:15:08 +0530

    conditional rendering
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
 import ImageSlider from "./components/ImageSlider";
+import Counter from "./components/Counter";
 class App extends Component {
+  state = {
+    visible: true,
+    whichComponentToShow: "slider",
+  };
   render() {
+    // if (this.state.whichComponentToShow === "slider") {
+    //   return <ImageSlider />;
+    // }
+    const buttonText = this.state.visible ? "Show Counter" : "Show ImageSlider";
     return (
       <div className="App">
-        <ImageSlider />
+        {this.state.visible ? <ImageSlider /> : <Counter initialCount={0} />}
+        <button
+          onClick={() =>
+            this.setState((state) => ({ visible: !state.visible }))
+          }>
+          {buttonText}
+        </button>
       </div>
     );
   }
