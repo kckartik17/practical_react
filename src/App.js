bf74b4902950445555baf41ebc744dec19b9c695
Kartik Chauhan <kckartik17@gmail.com>
2022-06-03 14:04:42 +0530

    component lifecycle
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
-import ImageSlider from "./components/ImageSlider";
 import Counter from "./components/Counter";
 class App extends Component {
   state = {
     visible: true,
-    whichComponentToShow: "slider",
   };
   render() {
-    // if (this.state.whichComponentToShow === "slider") {
-    //   return <ImageSlider />;
-    // }
-    const buttonText = this.state.visible ? "Show Counter" : "Show ImageSlider";
     return (
       <div className="App">
-        {this.state.visible ? <ImageSlider /> : <Counter initialCount={0} />}
-        <button
-          onClick={() =>
-            this.setState((state) => ({ visible: !state.visible }))
-          }>
-          {buttonText}
-        </button>
+        <div className={this.state.visible ? "visible" : "hidden"}>
+          <Counter initialCount={0} />
+        </div>
+        <div>
+          <button
+            onClick={() =>
+              this.setState((state) => ({
+                visible: !state.visible,
+              }))
+            }>
+            Toggle Counter
+          </button>
+        </div>
       </div>
     );
   }
